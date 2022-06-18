c31041593c723e1b3a5bf72b87f1f0e4921210b8
muellllkreis <rietig.m@googlemail.com>
2022-06-18 13:54:30 +0200

    bugfix attempt for hosted version3

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 let init = () => {
     const canvas = document.getElementById('fox-img');
     const ctx = canvas.getContext('2d');
-    canvas.width = $('img').width();
-    canvas.crossOrigin = "anonymous";
-    canvas.height = $('img').height();
-    ctx.drawImage($('img').get(0), 0, 0);
+    canvas.width = $('img.base-image').width();
+    canvas.crossOrigin = "Anonymous";
+    canvas.height = $('img.base-image').height();
+    ctx.drawImage($('img.base-image').get(0), 0, 0);
     ctx.font = '36pt "Code 7x5"';
     $(document).on('input','#inp',function(){
         //var ImageData = ctx.getImageData(0, 0, 0, 0);
         //console.log(ImageData);
         //redraw image
         ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage($('img').get(0), 0, 0);
+        ctx.drawImage($('img.base-image').get(0), 0, 0);
         //refill text
         ctx.fillStyle = 'white';
         ctx.fillRect(150, 50, 300, 100);
