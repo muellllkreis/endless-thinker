74b5b2d72c5cafa05c558919f251f6c05e718fbc
muellllkreis <rietig.m@googlemail.com>
2022-06-18 13:44:13 +0200

    text on image cross origin test

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let init = () => {
+    const canvas = document.getElementById('fox-img');
+    const ctx = canvas.getContext('2d');
+    canvas.width = $('img').width();
+    canvas.crossOrigin = "anonymous";
+    canvas.height = $('img').height();
+    ctx.drawImage($('img').get(0), 0, 0);
+    ctx.font = '36pt "Code 7x5"';
+    $(document).on('input','#inp',function(){
+        var ImageData = ctx.getImageData(0, 0, 0, 0);
+        console.log(ImageData);
+        //redraw image
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.drawImage($('img').get(0), 0, 0);
+        //refill text
+        ctx.fillStyle = 'white';
+        ctx.fillRect(150, 50, 300, 100);
+        ctx.fillStyle = "black";
+        ctx.fillText($(this).val(),150,150, 300);
+    });
+    $('button').click(function(){
+        console.log(ctx.getImageData(50, 50, 100, 100));
+    });
+}
+
+let myFunction = () => {
+    console.log("Clicked on Button!");
+}
+
+window.onload = init
