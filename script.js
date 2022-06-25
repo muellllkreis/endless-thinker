b64e5cb7bdc995a67f576a2851c16a65ad276bbc
LeonardoNatale <leonardo@multis.co>
2022-06-25 17:03:28 +0200

    removed firestore module

--- script.js
+++ script.js
@@ -1,97 +1,187 @@
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
 
 // constants
-const QUOTE_OFFSET = 75;  // pixel height of top space for quote
-const LINE_HEIGHT = 32;   // height of lines in quote text
-const MAX_LINES = 5;       // max lines allowed in quote text
+const QUOTE_OFFSET = 75; // pixel height of top space for quote
+const LINE_HEIGHT = 32; // height of lines in quote text
+const MAX_LINES = 5; // max lines allowed in quote text
 
 const init = () => {
-    // build canvas based on fox image
-    const canvas = document.getElementById('fox-img');
-    const ctx = canvas.getContext('2d');
-    const img = document.getElementById('fox-base-img');
-    canvas.crossOrigin = "anonymous";
+  // Search
 
-    // we add some height to make space for the quote
-    canvas.width = img.width;
-    canvas.height = img.height + QUOTE_OFFSET;
-    ctx.drawImage(img, 0, QUOTE_OFFSET);
+  const searchClient = algoliasearch(
+    "PYVVRN781V",
+    "1214904b737f1de7de8e9c7ae7227d3e"
+  );
 
-    // color definitions
-    // backgroundcolor: get topmost pixel of fox image
-    // namecolor: get bottom pixel in the middle of the image
-    // @TODO: namecolor does not work with every fox type
-    var backgroundColor = ctx.getImageData(0, QUOTE_OFFSET, 1, 1).data;
-    var nameColor = ctx.getImageData(canvas.width/2, canvas.height-1, 1, 1).data;
+  const search = instantsearch({
+    indexName: "demo_ecommerce",
+    searchClient,
+  });
 
-    // draw top rectangle
-    ctx.fillStyle =  "rgba(" + backgroundColor[0] + "," + backgroundColor[1] + "," + backgroundColor[2] + "," + backgroundColor[3] + ")";
-    ctx.fillRect(0, 0, 600, QUOTE_OFFSET);
+  search.addWidgets([
+    instantsearch.widgets.searchBox({
+      container: "#searchbox",
+    }),
 
-    // input field definitions
-    var thoughtText = "";
-    var nameText = "";
-    var nameInput = document.getElementById('name');
-    var thoughtInput = document.getElementById('thought');
+    instantsearch.widgets.hits({
+      container: "#hits",
+    }),
+  ]);
 
-    // input field listeners
-    nameInput.addEventListener("keyup", function (evt) {
-        // prevent enter from submitting the form
-        if (evt.code == "Enter") {
-            evt.stopPropagation();
-        }
-        nameText = nameInput.value;
-        thoughtText = thoughtInput.value;
-        drawText(canvas, ctx, img, nameText, thoughtText, backgroundColor, nameColor);
-    }, false);
+  search.start();
 
-    thoughtInput.addEventListener("keyup", function (evt) {
-        // prevent enter from submitting the form
-        if (evt.code == "Enter") {
-            evt.stopPropagation();
-        }
-        nameText = nameInput.value;
-        thoughtText = thoughtInput.value;
-        drawText(canvas, ctx, img, nameText, thoughtText, backgroundColor, nameColor);
-    }, false);
-}
+  // build canvas based on fox image
+  const canvas = document.getElementById("fox-img");
+  const ctx = canvas.getContext("2d");
+  const img = document.getElementById("fox-base-img");
+  canvas.crossOrigin = "anonymous";
 
-const drawText = (canvas, context, img, nameText, thoughtText, backgroundColor, nameColor) => {
-    //redraw image
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(img, 0, QUOTE_OFFSET);
+  // we add some height to make space for the quote
+  canvas.width = img.width;
+  canvas.height = img.height + QUOTE_OFFSET;
+  ctx.drawImage(img, 0, QUOTE_OFFSET);
 
-    // draw rectangle with on top for quote
-    // @TODO: make size dynamic depending on quote length
-    context.fillStyle =  "rgba(" + backgroundColor[0] + "," + backgroundColor[1] + "," + backgroundColor[2] + "," + backgroundColor[3] + ")";
-    context.fillRect(0, 0, 600, QUOTE_OFFSET);
+  // color definitions
+  // backgroundcolor: get topmost pixel of fox image
+  // namecolor: get bottom pixel in the middle of the image
+  // @TODO: namecolor does not work with every fox type
+  var backgroundColor = ctx.getImageData(0, QUOTE_OFFSET, 1, 1).data;
+  var nameColor = ctx.getImageData(
+    canvas.width / 2,
+    canvas.height - 1,
+    1,
+    1
+  ).data;
 
-    // refill text
-    // leave name if it hasn't been changed
-    if(nameText.length > 0) {
-        // rectangle that hides the original name
-        context.fillRect(150, 50, 300, 100 + QUOTE_OFFSET);
-    
-        // draw name text
-        // @TODO: something is wrong with the positioning here
-        context.fillStyle = "rgba(" + nameColor[0] + "," + nameColor[1] + "," + nameColor[2] + "," + nameColor[3] + ")";
-        context.font = '24pt "Code 7x5"';
-        context.fillText(nameText, 150, 150 + QUOTE_OFFSET/2, 300);
+  // draw top rectangle
+  ctx.fillStyle =
+    "rgba(" +
+    backgroundColor[0] +
+    "," +
+    backgroundColor[1] +
+    "," +
+    backgroundColor[2] +
+    "," +
+    backgroundColor[3] +
+    ")";
+  ctx.fillRect(0, 0, 600, QUOTE_OFFSET);
+
+  // input field definitions
+  var thoughtText = "";
+  var nameText = "";
+  var nameInput = document.getElementById("name");
+  var thoughtInput = document.getElementById("thought");
+
+  // input field listeners
+  nameInput.addEventListener(
+    "keyup",
+    function (evt) {
+      // prevent enter from submitting the form
+      if (evt.code == "Enter") {
+        evt.stopPropagation();
+      }
+      nameText = nameInput.value;
+      thoughtText = thoughtInput.value;
+      drawText(
+        canvas,
+        ctx,
+        img,
+        nameText,
+        thoughtText,
+        backgroundColor,
+        nameColor
+      );
+    },
+    false
+  );
+
+  thoughtInput.addEventListener(
+    "keyup",
+    function (evt) {
+      // prevent enter from submitting the form
+      if (evt.code == "Enter") {
+        evt.stopPropagation();
+      }
+      nameText = nameInput.value;
+      thoughtText = thoughtInput.value;
+      drawText(
+        canvas,
+        ctx,
+        img,
+        nameText,
+        thoughtText,
+        backgroundColor,
+        nameColor
+      );
+    },
+    false
+  );
+};
+
+const drawText = (
+  canvas,
+  context,
+  img,
+  nameText,
+  thoughtText,
+  backgroundColor,
+  nameColor
+) => {
+  //redraw image
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(img, 0, QUOTE_OFFSET);
+
+  // draw rectangle with on top for quote
+  // @TODO: make size dynamic depending on quote length
+  context.fillStyle =
+    "rgba(" +
+    backgroundColor[0] +
+    "," +
+    backgroundColor[1] +
+    "," +
+    backgroundColor[2] +
+    "," +
+    backgroundColor[3] +
+    ")";
+  context.fillRect(0, 0, 600, QUOTE_OFFSET);
+
+  // refill text
+  // leave name if it hasn't been changed
+  if (nameText.length > 0) {
+    // rectangle that hides the original name
+    context.fillRect(150, 50, 300, 100 + QUOTE_OFFSET);
+
+    // draw name text
+    // @TODO: something is wrong with the positioning here
+    context.fillStyle =
+      "rgba(" +
+      nameColor[0] +
+      "," +
+      nameColor[1] +
+      "," +
+      nameColor[2] +
+      "," +
+      nameColor[3] +
+      ")";
+    context.font = '24pt "Code 7x5"';
+    context.fillText(nameText, 150, 150 + QUOTE_OFFSET / 2, 300);
+  }
+
+  // draw thought text
+  context.fillStyle = "rgba(0, 0, 0, 255)";
+  context.font = '24pt "IBM Plex Mono"';
+  let lines = thoughtText.split("\n");
+
+  for (var i = 0; i < lines.length; i++) {
+    if (i + 1 < MAX_LINES) {
+      context.fillText(lines[i], 50, 40 + i * LINE_HEIGHT, 500);
     }
-
-    // draw thought text
-    context.fillStyle = "rgba(0, 0, 0, 255)";
-    context.font = '24pt "IBM Plex Mono"';
-    let lines = thoughtText.split('\n');
-
-    for (var i = 0; i < lines.length; i++) {
-        if(i + 1 < MAX_LINES) {
-            context.fillText(lines[i], 50, 40 + (i * LINE_HEIGHT), 500);
-        }
-    }
-}
+  }
+};
 
 const myFunction = () => {
-    console.log("Clicked on Button!");
-}
+  console.log("Clicked on Button!");
+};
 
-window.onload = init
+window.onload = init;
